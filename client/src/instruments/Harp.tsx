459d20ddc864d4b4d98f873aaacc2e94fc47bab8
CVTorrez <CVTorrez@gmail.com>
2023-04-27 19:46:38 -0700

    harp update
    fixed paths and objects

--- client/src/instruments/Harp.tsx
+++ client/src/instruments/Harp.tsx
@@ -14,6 +14,7 @@ import { Instrument, InstrumentProps } from '../Instruments';
  ** ------------------------------------------------------------------------ */
 
 const Harp: React.FC<InstrumentProps> = ({synth, setSynth}) => {
+  const harpStrings = [
   {path: require("../HarpNotes/a2-ksharp_a2_mf.wav"), label: "a2Sharp"},
   {path: require("../HarpNotes/a4-ksharp_a4_mf.wav"), label: "a4Sharp"},
   {path: require("../HarpNotes/a6-ksharp_a6_mf.wav"), label: "a6Sharp"},
@@ -39,171 +40,200 @@ const Harp: React.FC<InstrumentProps> = ({synth, setSynth}) => {
 ];
 
   const [player, setPlayers] = useState<Tone.Player[]>([]);
-  const stringClick = (player: Tone.Player) => {
-    player.start();    
-  }
+  
+  useEffect(() => {
+    setPlayers(harpStrings.map((Harp) => loadSample(Harp.path)));
+  }, []);
+
+  const loadSample = (url: string) => {
+    return new Tone.Player(url).toDestination();
+  };
+
+  const handleStringClick = (player: Tone.Player) => {
+    player.start();
+  };
+  return (
+    <div className="Harp">
+      <div className="outer-grid">
+        <div className="grid">
+          {Harp.map((Harp, index) => {
+            const player = loadSample(Harp.path);
+            return (
+              <button
+                key={index}
+                onClick={() => handleStringClick(player)}
+                className="Harp"
+              >
+                {Harp.label}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
-interface HarpKeyProps {
-  note: string; // C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B
-  duration?: string;
-  synth?: Tone.Synth; // Contains library code for making sound
-  minor?: boolean; // True if minor key, false if major key
-  octave: number;
-  index: number; // octave + index together give a location for the Harp key
-}
+// interface HarpKeyProps {
+//   note: string; // C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B
+//   duration?: string;
+//   synth?: Tone.Synth; // Contains library code for making sound
+//   minor?: boolean; // True if minor key, false if major key
+//   octave: number;
+//   index: number; // octave + index together give a location for the Harp key
+// }
 
-export function HarpKey({
-  note,
-  synth,
-  minor,
-  index,
-}: HarpKeyProps): JSX.Element {
+// export function HarpKey({
+//   note,
+//   synth,
+//   minor,
+//   index,
+// }: HarpKeyProps): JSX.Element {
   /**
    * This React component corresponds to either a major or minor key in the Harp.
    * See `HarpKeyWithoutJSX` for the React component without JSX.
    */
-  return (
-    // Observations:
-    // 1. The JSX refers to the HTML-looking syntax within TypeScript.
-    // 2. The JSX will be **transpiled** into the corresponding `React.createElement` library call.
-    // 3. The curly braces `{` and `}` should remind you of string interpolation.
-    <div
-      onMouseDown={() => synth?.triggerAttack(`${note}`)} // Question: what is `onMouseDown`?
-      onMouseUp={() => synth?.triggerRelease('+0.25')} // Question: what is `onMouseUp`?
-      className={classNames('ba pointer absolute dim', {
-        'bg-black black h3': minor, // minor keys are black
-        'black bg-white h4': !minor, // major keys are white
-      })}
-      style={{
-        // CSS
-        top: 0,
-        left: `${index * 2}rem`,
-        zIndex: minor ? 1 : 0,
-        width: minor ? '1.5rem' : '2rem',
-        marginLeft: minor ? '0.25rem' : 0,
-      }}
-    ></div>
-  );
-}
+//   return (
+//     // Observations:
+//     // 1. The JSX refers to the HTML-looking syntax within TypeScript.
+//     // 2. The JSX will be **transpiled** into the corresponding `React.createElement` library call.
+//     // 3. The curly braces `{` and `}` should remind you of string interpolation.
+//     <div
+//       onMouseDown={() => synth?.triggerAttack(`${note}`)} // Question: what is `onMouseDown`?
+//       onMouseUp={() => synth?.triggerRelease('+0.25')} // Question: what is `onMouseUp`?
+//       className={classNames('ba pointer absolute dim', {
+//         'bg-black black h3': minor, // minor keys are black
+//         'black bg-white h4': !minor, // major keys are white
+//       })}
+//       style={{
+//         // CSS
+//         top: 0,
+//         left: `${index * 2}rem`,
+//         zIndex: minor ? 1 : 0,
+//         width: minor ? '1.5rem' : '2rem',
+//         marginLeft: minor ? '0.25rem' : 0,
+//       }}
+//     ></div>
+//   );
+// }
 
-// eslint-disable-next-line
-function HarpKeyWithoutJSX({
-  note,
-  synth,
-  minor,
-  index,
-}: HarpKeyProps): JSX.Element {
-  /**
-   * This React component for pedagogical purposes.
-   * See `HarpKey` for the React component with JSX (JavaScript XML).
-   */
-  return React.createElement(
-    'div',
-    {
-      onMouseDown: () => synth?.triggerAttack(`${note}`),
-      onMouseUp: () => synth?.triggerRelease('+0.25'),
-      className: classNames('ba pointer absolute dim', {
-        'bg-black black h3': minor,
-        'black bg-white h4': !minor,
-      }),
-      style: {
-        top: 0,
-        left: `${index * 2}rem`,
-        zIndex: minor ? 1 : 0,
-        width: minor ? '1.5rem' : '2rem',
-        marginLeft: minor ? '0.25rem' : 0,
-      },
-    },
-    [],
-  );
-}
+// // eslint-disable-next-line
+// function HarpKeyWithoutJSX({
+//   note,
+//   synth,
+//   minor,
+//   index,
+// }: HarpKeyProps): JSX.Element {
+//   /**
+//    * This React component for pedagogical purposes.
+//    * See `HarpKey` for the React component with JSX (JavaScript XML).
+//    */
+//   return React.createElement(
+//     'div',
+//     {
+//       onMouseDown: () => synth?.triggerAttack(`${note}`),
+//       onMouseUp: () => synth?.triggerRelease('+0.25'),
+//       className: classNames('ba pointer absolute dim', {
+//         'bg-black black h3': minor,
+//         'black bg-white h4': !minor,
+//       }),
+//       style: {
+//         top: 0,
+//         left: `${index * 2}rem`,
+//         zIndex: minor ? 1 : 0,
+//         width: minor ? '1.5rem' : '2rem',
+//         marginLeft: minor ? '0.25rem' : 0,
+//       },
+//     },
+//     [],
+//   );
+// }
 
-function HarpType({ title, onClick, active }: any): JSX.Element {
-  return (
-    <div
-      onClick={onClick}
-      className={classNames('dim pointer ph2 pv1 ba mr2 br1 fw7 bw1', {
-        'b--black black': active,
-        'gray b--light-gray': !active,
-      })}
-    >
-      {title}
-    </div>
-  );
-}
+// function HarpType({ title, onClick, active }: any): JSX.Element {
+//   return (
+//     <div
+//       onClick={onClick}
+//       className={classNames('dim pointer ph2 pv1 ba mr2 br1 fw7 bw1', {
+//         'b--black black': active,
+//         'gray b--light-gray': !active,
+//       })}
+//     >
+//       {title}
+//     </div>
+//   );
+// }
 
-function Harp({ synth, setSynth }: InstrumentProps): JSX.Element {
-  const keys = List([
-    { note: 'C', idx: 0 },
-    { note: 'Db', idx: 0.5 },
-    { note: 'D', idx: 1 },
-    { note: 'Eb', idx: 1.5 },
-    { note: 'E', idx: 2 },
-    { note: 'F', idx: 3 },
-    { note: 'Gb', idx: 3.5 },
-    { note: 'G', idx: 4 },
-    { note: 'Ab', idx: 4.5 },
-    { note: 'A', idx: 5 },
-    { note: 'Bb', idx: 5.5 },
-    { note: 'B', idx: 6 },
-  ]);
+// function Harp({ synth, setSynth }: InstrumentProps): JSX.Element {
+//   const keys = List([
+//     { note: 'C', idx: 0 },
+//     { note: 'Db', idx: 0.5 },
+//     { note: 'D', idx: 1 },
+//     { note: 'Eb', idx: 1.5 },
+//     { note: 'E', idx: 2 },
+//     { note: 'F', idx: 3 },
+//     { note: 'Gb', idx: 3.5 },
+//     { note: 'G', idx: 4 },
+//     { note: 'Ab', idx: 4.5 },
+//     { note: 'A', idx: 5 },
+//     { note: 'Bb', idx: 5.5 },
+//     { note: 'B', idx: 6 },
+//   ]);
 
-  const setOscillator = (newType: Tone.ToneOscillatorType) => {
-    setSynth(oldSynth => {
-      oldSynth.disconnect();
+//   const setOscillator = (newType: Tone.ToneOscillatorType) => {
+//     setSynth(oldSynth => {
+//       oldSynth.disconnect();
 
-      return new Tone.Synth({
-        oscillator: { type: newType } as Tone.OmniOscillatorOptions,
-      }).toDestination();
-    });
-  };
+//       return new Tone.Synth({
+//         oscillator: { type: newType } as Tone.OmniOscillatorOptions,
+//       }).toDestination();
+//     });
+//   };
 
-  const oscillators: List<OscillatorType> = List([
-    'sine',
-    'sawtooth',
-    'square',
-    'triangle',
-    'fmsine',
-    'fmsawtooth',
-    'fmtriangle',
-    'amsine',
-    'amsawtooth',
-    'amtriangle',
-  ]) as List<OscillatorType>;
+//   const oscillators: List<OscillatorType> = List([
+//     'sine',
+//     'sawtooth',
+//     'square',
+//     'triangle',
+//     'fmsine',
+//     'fmsawtooth',
+//     'fmtriangle',
+//     'amsine',
+//     'amsawtooth',
+//     'amtriangle',
+//   ]) as List<OscillatorType>;
 
-  return (
-    <div className="pv4">
-      <div className="relative dib h4 w-100 ml4">
-        {Range(2, 7).map(octave =>
-          keys.map(key => {
-            const isMinor = key.note.indexOf('b') !== -1;
-            const note = `${key.note}${octave}`;
-            return (
-              <HarpKey
-                key={note} //react key
-                note={note}
-                synth={synth}
-                minor={isMinor}
-                octave={octave}
-                index={(octave - 2) * 7 + key.idx}
-              />
-            );
-          }),
-        )}
-      </div>
-      <div className={'pl4 pt4 flex'}>
-        {oscillators.map(o => (
-          <HarpType
-            key={o}
-            title={o}
-            onClick={() => setOscillator(o)}
-            active={synth?.oscillator.type === o}
-          />
-        ))}
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="pv4">
+//       <div className="relative dib h4 w-100 ml4">
+//         {Range(2, 7).map(octave =>
+//           keys.map(key => {
+//             const isMinor = key.note.indexOf('b') !== -1;
+//             const note = `${key.note}${octave}`;
+//             return (
+//               <HarpKey
+//                 key={note} //react key
+//                 note={note}
+//                 synth={synth}
+//                 minor={isMinor}
+//                 octave={octave}
+//                 index={(octave - 2) * 7 + key.idx}
+//               />
+//             );
+//           }),
+//         )}
+//       </div>
+//       <div className={'pl4 pt4 flex'}>
+//         {oscillators.map(o => (
+//           <HarpType
+//             key={o}
+//             title={o}
+//             onClick={() => setOscillator(o)}
+//             active={synth?.oscillator.type === o}
+//           />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
 
 export const HarpInstrument = new Instrument('Harp', Harp);
