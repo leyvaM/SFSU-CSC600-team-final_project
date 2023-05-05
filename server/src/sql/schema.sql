777dffbf506619e1e507b67573d7f3610185cf47
KevBarbour15 <89958027+KevBarbour15@users.noreply.github.com>
2023-05-04 17:35:55 -0700

    add random song picker

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -15,3 +15,12 @@ VALUES (3, 'Mary Had a Little Lamb', 'E4 D4 C4 D4 E4 E4 E4 D4 D4 D4 E4 G4 G4 E4 
 
 INSERT INTO songs (id, song_title, notes)
 VALUES (4, 'Jingle Bells', 'E4 E4 E4 E4 E4 E4 E4 G4 C4 D4 E4 F4 F4 F4 F4 F4 E4 E4 E4 E4 E4 D4 D4 E4 D4 G4');
+
+INSERT INTO songs (id, song_title, notes)
+VALUES (5, 'Happy Birthday', 'C4 C4 D4 C4 F4 E4 C4 C4 D4 C4 G4 F4 C4 C4 C5 A4 F4 E4 D4 A4 A4 A4 F4 G4 F4');
+
+INSERT INTO songs (id, song_title, notes)
+VALUES (6, 'We Wish You a Merry Christmas', 'G4 G4 A4 G4 C5 B4 G4 G4 A4 G4 D5 C5 G4 G4 G5 E5 C5 B4 A4 F5 F5 E5 C5 D5 C5');
+
+INSERT INTO songs (id, song_title, notes)
+VALUES (7, 'The Entertainer', 'D4 E4 D4 G4 C4 B4 A4 G4 E4 D4 E4 D4 G4 C4 B4 A4 G4 E4 D4 G4 D4 E4 D4 G4 C4 B4 A4 G4 E4 D4 E4 D4 G4 C4 B4 A4 G4 E4 D4 G4 D4 E4 D4 G4 C4 B4 A4 G4 E4 D4');
