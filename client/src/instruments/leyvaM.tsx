d002d897cc94549f4f60ca573e56acec6071ca8a
Mario Leyva Moreno <leyvam526@gmail.com>
2023-05-07 23:45:18 -0700

    updated guitar layout with clickable notes

--- client/src/instruments/leyvaM.tsx
+++ client/src/instruments/leyvaM.tsx
@@ -4,13 +4,150 @@ import classNames from 'classnames';
 import React, { useEffect, useMemo, useState } from 'react';
 import { List, Range } from 'immutable';
 // More 3rd party library imports
-import Guitar, {getRenderFingerSpn} from 'react-guitar';
+import Guitar, { getRenderFingerSpn } from 'react-guitar';
 import { standard } from "react-guitar-tunings";
 import useSound from "react-guitar-sound";
 // project imports
 import { Instrument, InstrumentProps } from "../Instruments";
+import { type } from 'os';
+import { spanishTheme } from 'react-guitar';
 
+interface GuitarKeyProps {
+  note: string;
+  //duration?: string;
+  synth: Tone.Synth;
+  //setSynth: React.Dispatch<React.SetStateAction<Tone.Synth>>;
+  fret: number;
+}
 
+export function GuitarKey({
+  note,
+  synth,
+  fret,
+  //string,
+}: GuitarKeyProps): JSX.Element {
+  const [active, setActive] = useState(false);
+
+  const onStringClick = () => {
+    setActive(true);
+    synth?.triggerAttackRelease(`${note}${fret}`, '8n');
+  };
+
+  const onStringRelease = () => {
+    setActive(false);
+  }
+
+  return (
+    <div
+      onMouseDown={onStringClick}
+      onMouseUp={onStringRelease}
+      className={classNames('ba pointer absolute dim', {
+        'bg-black black h3': active,
+        'black bg-white h4': !active,
+      })}
+      style={{
+        // CSS
+        top: 0,
+        left: `${fret * 2}rem`,
+        zIndex: active ? 1 : 0,
+        width: active ? '1.5rem' : '2rem',
+        marginLeft: active ? '0.25rem' : 0,
+      }}
+    ></div>
+  );
+}
+
+function MyGuitar({ synth, setSynth}: InstrumentProps): JSX.Element {
+  const tuning = ['E2', 'A2', 'D3', 'G3', 'B3', 'E4'];
+  const frets = { from: 0, amount: 20 }; // modify to define the number of frets to show
+  const [strings, setStrings] = useState(tuning.map(note => Tone.Midi(note).toNote())); // convert note strings to MIDI notes
+  const renderFinger = getRenderFingerSpn(strings.map(note => Tone.Midi(note).toMidi())); // convert MIDI notes to numbers for `react-guitar`
+
+  useEffect(() => {
+    setSynth((oldSynth) => {
+      // update the synth whenever the strings state changes
+      oldSynth?.dispose();
+      return new Tone.Synth().toDestination();
+    });
+    // update the synth whenever the strings state changes
+  }, [setSynth]);
+
+  const onStringChange = (newStrings: number[]) => {
+    setStrings(newStrings.map(note => Tone.Midi(note).toNote()));
+  };
+
+  return (
+    <div className='flex flex-column items-center'>
+      <Guitar
+        id='my-guitar'
+        strings={strings.map(note => Tone.Midi(note).toMidi())} // convert note strings to MIDI notes
+        frets={frets}
+        renderFinger={renderFinger}
+        theme={spanishTheme}
+        onChange={onStringChange}
+        onPlay={string => synth?.triggerAttackRelease(Tone.Midi(strings[string]).toNote(), '8n')}
+      />
+      <div className='flex items-center mt3'>
+        {tuning.map((note, index) => (
+          <GuitarKey
+            key={index}
+            note={note}
+            synth={synth}
+            //setSynth={setSynth}
+            fret={0}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+/*
+function GuitarType({ title, onClick, active }: any): JSX.Element {
+  return (
+    <div
+      onClick={onClick}
+      className={classNames('dim pointer ph2 pv1 ba mr2 br1 fw7 bw1', {
+        'b--black black': active,
+        'gray b--light-gray': !active,
+      })}
+    >
+      {title}
+    </div>
+  );
+}*/
+
+/*
+function MyGuitar({ synth, setSynth }: InstrumentProps): JSX.Element {
+  const tuning = ['E2', 'A2', 'D3', 'G3', 'B3', 'E4'];
+  const frets = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+  const renderFingerSpan = getRenderFingerSpn(frets);
+
+  return (
+    <div className='relative dib h4 w-100 ml4'>
+      <Guitar
+      tuning={tuning}
+      frets={{from: 0, amount:20}}
+      renderKey={({string, fret}) => (
+        <GuitarKey
+        key={`${string}${fret}`}
+        note={tuning[string] + fret}
+        synth={synth}
+        setSynth={setSynth}
+        fret={fret}
+        string={string}
+        />
+      )}
+      renderFingerSpan={renderFingerSpan}
+      width={800}
+      height={300}
+      />
+    </div>
+  );
+}
+*/
+
+/*
 function MyGuitar({ synth, setSynth }: InstrumentProps): JSX.Element {
   const strings = useMemo(() => [0, 0, 0, 0, 0, 0], []);
   const { play } = useSound({ fretting: strings, tuning: standard });
@@ -26,6 +163,7 @@ function MyGuitar({ synth, setSynth }: InstrumentProps): JSX.Element {
     </div>
   )
 }
+*/
 
 export const GuitarInstrument = new Instrument('Guitar', MyGuitar);
 
