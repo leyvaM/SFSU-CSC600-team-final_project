f3a026cfb0e56141b1543e2888df935f57f05763
CVTorrez <CVTorrez@gmail.com>
2023-05-14 23:01:04 -0700

    modified colorform
    modified visualizer

--- client/src/visualizers/ColorForm.tsx
+++ client/src/visualizers/ColorForm.tsx
@@ -1,10 +1,14 @@
-// 3rd party library imports
+// 3rd party library imports; 
 import P5 from 'p5';
 import * as Tone from 'tone';
 
 // project imports
 import { Visualizer } from '../Visualizers';
 
+// function randomColor() {
+//   let o = Math.round, r = Math.random, s = 255;
+//   return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + 0.5 + ')';
+// }
 
 export const ColorFormVisualizer = new Visualizer(
   'ColorForm',
@@ -16,19 +20,17 @@ export const ColorFormVisualizer = new Visualizer(
     p5.background(100, 100, 100, 255);
 
     p5.strokeWeight(dim * 0.01);
-    //p5.stroke(255, 0, 0, 255);
+    p5.stroke('red');
     p5.noFill();
 
     const values = analyzer.getValue();
     p5.beginShape();
     for (let i = 0; i < values.length; i++) {
       const amplitude = values[i] as number;
-      //console.log("Amplitude" + amplitude);
-      const x = p5.map(i, 0, values.length - 1, 0, width);
-      const y = height / 2 + amplitude * (height * 5);
+      const y = p5.map(i, 0, values.length - 1, 0, width);
+      const x = height / 2 + amplitude * (height * 5);
       // Place vertex
       p5.vertex(x, y);
-      //p5.stroke(height, 255, 255, 255);
     }
     p5.endShape();
   },
