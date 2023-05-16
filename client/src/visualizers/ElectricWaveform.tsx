911b098c272bd7aa71a60aca5bf973335db2ec6c
CVTorrez <CVTorrez@gmail.com>
2023-05-15 21:31:46 -0700

    updated name
    changed "color form" to "electric form" on display

--- client/src/visualizers/ElectricWaveform.tsx
+++ client/src/visualizers/ElectricWaveform.tsx
@@ -12,7 +12,7 @@ import { Visualizer } from '../Visualizers';
 // }
 
 export const ElectricWaveform = new Visualizer(
-  'ColorForm',
+  'Electric Form',
   (p5: P5, analyzer: Tone.Analyser) => {
     const width = window.innerWidth;
     const height = window.innerHeight / 2;
